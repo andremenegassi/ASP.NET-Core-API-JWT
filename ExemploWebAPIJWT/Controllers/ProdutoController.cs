5f0617009206c5ab01f9a799081fc628c491a4f0
André Menegassi <andremenegassi@hotmail.com>
2019-04-24 23:10:48 -0300

    Autorização da ProdutoController.

--- ExemploWebAPIJWT/Controllers/ProdutoController.cs
+++ ExemploWebAPIJWT/Controllers/ProdutoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,7 @@ namespace ExemploWebAPIJWT.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize("Bearer")]
     public class ProdutoController : ControllerBase
     {
 
